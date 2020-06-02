0555586d5c133f645ce0fdac0fe3485a1519505f
mlopezcruz2015 <mlopezcruz2015@fau.edu>
2020-06-02 19:31:25 -0400

    no message

--- app/src/main/java/com/example/group6/MainMenuFragment.java
+++ app/src/main/java/com/example/group6/MainMenuFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+public class MainMenuFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,17 +17,17 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_mainmenu, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_settings).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                NavHostFragment.findNavController(MainMenuFragment.this)
+                        .navigate(R.id.action_MainMenuFragment_to_SettingsFragment);
             }
         });
     }
